7bbcbe0bae1ee56984aafd05bb7fe79e9335f427
lauren haas <laurenraehaas@gmail.com>
2023-08-09 21:09:31 -0500

    working on models

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -6,47 +6,4 @@ CREATE DATABASE ecommerce_db;
 
 USE ecommerce_db;
 
-CREATE TABLE Category (
-    id {
-        type: INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    }
-    category_name {
-        type: STRING NOT NULL,
-    }
-),
 
-CREATE TABLE Products (
-    id {
-        type: INT NOT NULL PRIMARY KEY AUTO_INCREMENT
-    }
-    product_name {
-        type: STRING NOT NULL,
-    } 
-    price {
-        type: DECIMAL NOT NULL VALIDATE,
-    } 
-    stock {
-        type: INT NOT NULL DEFAULT '10' VALIDATE,
-    } 
-    category_id {
-        type: INT,
-        references: {
-            
-        }
-    }
-),
-
-CREATE TABLE Tag (
-    id {
-        type: INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    }
-    tag_name {
-        type: STRING,
-    }
-),
-
-CREATE TABLE ProductTag (
-    id {
-        type 
-    }
-)
--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,15 +6,32 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
   },
+  product_id: {
+    references: {
+      model: 'Product',
+      key: 'id',
+    },
+  },
+  tag_id: {
+    references: {
+      model: 'Tag',
+      key: id,
+    },
+  },
+},
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'product_tag',
-  }
+  },
 );
 
 module.exports = ProductTag;
